bd0f70b01f132ae268e3af9426a058cb635b53b1
Oleg Lusan <apovis@grafmurr.de>
2016-10-29 12:13:17 +0200

    updates to 4.8.5 no grsec for soeki!

---
+++ change_version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+KERNEL="4.8"
+
+echo "date:"
+read DATE_NEW
+
+echo "old kernel:"
+read KVER_OLD
+echo "new kernel version:"
+read KVER_NEW
+
+echo "grsec's new timestamp:"
+read TIMESTAMP_NEW
+
+
+sed -i "s|date=.*|date=${DATE_NEW}|g" PKGBUILD.*
+
+sed -i "s|pkgver=.*|pkgver=${KVER_NEW}|" PKGBUILD.*
+sed -i "s|${KVER_OLD}|${KVER_NEW}|" linux-titanium.install*
+
+sed -i "s|_timestamp=.*|_timestamp=${TIMESTAMP_NEW}|" PKGBUILD.*
+
+exit 0
